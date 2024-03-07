c333985281ed2ada8a83544cd274ff574f7440cf
TMPkl <karol.leszynski1@gmail.com>
2024-03-07 15:58:33 +0100

    v1.1

--- TestDataVtype.py
+++ TestDataVtype.py
@@ -1,18 +1,20 @@
 import random
 import itertools
+def genA(n):
 
-n = int(input())
+    n = int(input())
 
-L = [random.randrange(0,n*10,2) for _ in range(n//2)]
-R = [random.randrange(1,n*10,2) for _ in range(n-len(L))]
-5
-L.sort()
-R.sort(reverse=True)
-
-
-str_from_tab = str(list(itertools.chain(R,L)))[1:-1].replace(",","").replace(" ","\n")
+    L = [random.randrange(0,n*10,2) for _ in range(n//2)]
+    R = [random.randrange(1,n*10,2) for _ in range(n-len(L))]
+    
+    L.sort()
+    R.sort(reverse=True)
 
 
-f=open("test_data_V.txt","w")
-f.write(str_from_tab)
-print(list(itertools.chain(L,R)))
+    str_from_tab = str(list(itertools.chain(R,L)))[1:-1].replace(",","").replace(" ","\n")
+
+
+    f=open("test_data_V.txt","w")
+    f.write(str_from_tab)
+    f.close()
+    return list(itertools.chain(R,L))
