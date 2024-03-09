72feaf42144be8f94cefa4b5022cdbbfcdb3fde7
TMPkl <karol.leszynski1@gmail.com>
2024-03-09 18:19:54 +0100

    qs

--- quick_sort.py
+++ quick_sort.py
@@ -1,38 +1,46 @@
-def quick_sort(lst,start,stop):
-    if len(lst) == 1:
-        return 
-    i = start
-    j = piv = stop
-    j-=1
-    while i <= j:
-        while lst[i] < lst[piv] and i <= j and lst[j] > lst[piv]:
-            i+=1            ############################ tutaj muszę to robic po kroku a nie max i min j 
-            if lst[i] < lst[piv] and lst[j] > lst[piv]:
-                break
-            j+=1
-        lst[i], lst[j] = lst[j], lst[i]
-        i+=1
-        j-=1
-    if j-1 <= start:
-        if start+1 <= stop:
-            return start+1
-        else:
-            return stop-1
-    
-    return j-1
+import subprocess
+import timeit
+def podzial(lst, first, last):
+    pivot = lst[last]
+    i = first - 1
+    for j in range(first, last):
+        if lst[j] <= pivot:
+            i += 1
+            lst[i], lst[j] = lst[j], lst[i]
+    lst[i + 1], lst[last] = lst[last], lst[i + 1]
+    return i + 1
 
-A = [1, 8, 3, 4 , 5, 1, 6, 2]
-print(A)
-new_split_point = len(A)-1
-wektor = [-1,len(A)-1]
-quick_sort(A,0,len(A)-1)
-for x in range(len(A)-1):
-    nowy_wektor = [-1,]
-    for y in range(len(wektor)-1):
-        new_split_point = quick_sort(A, wektor[y]+1, wektor[y+1])
-        nowy_wektor.append(new_split_point)
-    print(A,wektor)
-    nowy_wektor.append(len(A)-1)
-    nowy_wektor.sort()
-    wektor = nowy_wektor
-print(A)
+def quicksort_iteracyjnie(lst):
+    if len(lst) <= 1:
+        return lst
+
+    stos = [(0, len(lst) - 1)]
+    while stos:
+        first, last = stos.pop()
+        if first < last:
+            pi = podzial(lst, first, last)
+            stos.append((first, pi - 1))
+            stos.append((pi + 1, last))
+    return lst
+
+if len(sys.argv) == 2:
+    subprocess.run(["python", "incTestData.py", sys.argv[1]])              ############## zmiana algo generującego
+    f = open("test_data_I.txt","r")                                        ############# zmiana pliku gdzie czytać dane
+    A = f.read().replace(",","").split(" ")
+    f.close()
+    A = [int(x) for x in A]
+    t0 =timeit.default_timer()
+    quicksort_iteracyjnie(A)
+    t1 = timeit.default_timer()
+    results_m = open("merge_sort_results.txt", "a")   
+    results_m.write(str(t1-t0)+"\n")
+    results_m.close()
+else:
+    A = input("podaj ilość danych testowych: ")          ##### jakie dane wygenerować 
+    A = A.split(" ")
+    A = [int(x) for x in A]
+   
+    t0 =timeit.default_timer()
+    quicksort_iteracyjnie(A)
+    t1 = timeit.default_timer()
+    print(A,"\n","w czasie: ",t1-t0)
