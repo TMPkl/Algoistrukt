068193f33076a40aafb5374c86ce2f2a97f9469a
TMPkl <karol.leszynski1@gmail.com>
2024-03-11 19:31:28 +0100

    szellsort

--- shell_sort/shellSort.py
+++ shell_sort/shellSort.py
@@ -1,1 +1,18 @@
-print("oifoisd")
+def shellSort(array, n):
+    interval = n // 2
+
+    while interval > 0:
+        i = interval
+        while i < n:
+            temp = array[i]
+            j = i
+            while j >= interval and array[j - interval] > temp:
+                array[j] = array[j - interval]
+                j -= interval
+
+            array[j] = temp
+            i += 1
+        interval //= 2
+    return array
+
+A = [3, 2, 4, 1]
