18944d440305b5c38f12215d5ad4476146fde3fc
TMPkl <karol.leszynski1@gmail.com>
2024-03-21 16:53:23 +0100

    zadanie a

--- zad2/BST.py
+++ zad2/BST.py
@@ -22,6 +22,24 @@ for a in A:
     root = insert(root, a)
 
 
+def find_smallest(node):
+    if node.left is None:
+        print(node.key)
+        return node.key
+    else:
+        print(node.key,end="-->")
+        find_smallest(node.left)
+
+def find_largest(node):
+    if node.right is None:
+        print(node.key)
+        return node.key
+    else:
+        print(node.key,end="-->")
+        return find_largest(node.right)
+
+
+
 def print_tree_preorder(node):
     if node is None:
         return
@@ -30,3 +48,7 @@ def print_tree_preorder(node):
     print_tree_preorder(node.right)
 
 print_tree_preorder(root)
+print("##################################################")
+find_smallest(root)
+print("##################################################")
+find_largest(root)
