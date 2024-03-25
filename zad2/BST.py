38b1c945aa57174d22a6af4f640664601eb5d015
TMPkl <karol.leszynski1@gmail.com>
2024-03-25 19:43:04 +0100

    BST bez ostantiego zadania

--- zad2/BST.py
+++ zad2/BST.py
@@ -15,7 +15,7 @@ def insert(node, key):
     
     return node
 
-A = [8,11,2,52,0,4,6,5]
+A = [8,11,2,0,4,52,5,6]
 
 root = None
 for a in A:
@@ -43,12 +43,33 @@ def find_largest(node):
 def print_tree_preorder(node):
     if node is None:
         return
-    print(node.key)
+    print(node.key,end=" -> ")
     print_tree_preorder(node.left)
     print_tree_preorder(node.right)
 
-print_tree_preorder(root)
-print("##################################################")
-find_smallest(root)
-print("##################################################")
-find_largest(root)
+def print_tree_sorted(node):
+    if node is None:
+        return
+    print_tree_sorted(node.left)
+    print(node.key)
+    print_tree_sorted(node.right)
+
+def print_pre_subtree(node,key):
+    if node is None:
+        return
+    if key == node.key:
+        print_tree_preorder(node)
+    else:
+        print_pre_subtree(node.left,key)
+        print_pre_subtree(node.right,key)
+
+# print("print_tree_preorder","##################################################")
+# print_tree_preorder(root)
+# print("find_smallest","##################################################")
+# find_smallest(root)
+# print("find_largest","##################################################")
+# find_largest(root)
+# print("print_tree_sorted","##################################################")
+# print(print_tree_sorted(root))
+# print("print_subtree_preordered(root,key)","##################################################")    
+#print_pre_subtree(root,2)
